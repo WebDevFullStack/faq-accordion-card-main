804a33bf7702d20c06d79d04a7b8a7fe6fa20045
accountnebula <admin@accountnebula.com>
2020-12-10 05:15:49 +0000

    basic answer displayed implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 // Faq Section
 //// add an event listener to each question wrapper
 ////
+
+var questionWrapper = document.querySelectorAll(".question-wrapper");
+
+for (var i = 0; i < questionWrapper.length; i++) {
+    questionWrapper[i].addEventListener("click", questionClicked);
+}
+
+function questionClicked() {
+    var answer = this.querySelector(".answer");
+    answer.classList.toggle("displayed");
+    answer.classList.toggle("hidden");
+}
